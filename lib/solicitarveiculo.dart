7cd5eed56ae4690f0f00c2ad3ca1a9520483f3f0
jmenarin <jpmenarin@icloud.com>
2021-10-26 17:30:24 -0300

    teste

--- lib/solicitarveiculo.dart
+++ lib/solicitarveiculo.dart
@@ -11,6 +11,7 @@ class _SolicitarVeiculoState extends State<SolicitarVeiculo> {
   TextEditingController testecontroller = TextEditingController();
   String valoresInicio = testar.last;
   int a = 6;
+  String ver = "";
 
   @override
   Widget build(BuildContext context) {
@@ -29,68 +30,74 @@ class _SolicitarVeiculoState extends State<SolicitarVeiculo> {
                           child: Text("Descreva as informações do veiculo")),
                       actions: [
                         Container(
-                          color: Colors.black12,
-                          height: 30,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          child: Column(
                             children: [
-                              Text("Nome"),
-                              DropdownButton<String>(
-                                  value: valoresInicio,
-                                  onChanged: (String? value) {
-                                    setState(() {
-                                      valoresInicio = value!;
-                                    });
-                                  },
-                                  items: testar.map((value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList()),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          color: Colors.black12,
-                          height: 30,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text("Nome"),
-                              DropdownButton<String>(
-                                  value: valoresInicio,
-                                  onChanged: (String? value) {
-                                    setState(() {
-                                      valoresInicio = value!;
-                                    });
-                                  },
-                                  items: testar.map((value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList()),
-                            ],
-                          ),
-                        ),
-                        Container(
-                            color: Colors.black12,
-                            height: 30,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text("Nome"),
-                                Container(
-                                  height: 20,
-                                  width: 100,
-                                  child: TextFormField(
-                                    decoration:
-                                        InputDecoration(hintText: "OLa"),
-                                  ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                height: 100,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.1,
+                                      child: Column(
+                                        children: [
+                                          Text("Cracha"),
+                                          Container(
+                                            width: 100,
+                                            height: 30,
+                                            child: TextFormField(
+                                              controller: testecontroller,
+                                              textAlign: TextAlign.center,
+                                              decoration: InputDecoration(
+                                                border: OutlineInputBorder(),
+                                                contentPadding:
+                                                    EdgeInsets.symmetric(
+                                                        horizontal: 10),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.1,
+                                      child: Column(
+                                        children: [
+                                          Text("Ola"),
+                                          Container(
+                                            height: 35,
+                                            child: Text(ver),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.1,
+                                      child: Column(
+                                        children: [Text("Ola"), Text("3")],
+                                      ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.1,
+                                      child: Column(
+                                        children: [Text("Ola"), Text("4")],
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            )),
+                              ),
+                              IconButton(
+                                  onPressed: () {
+                                    ver = testecontroller.text;
+                                  },
+                                  icon: Icon(Icons.add))
+                            ],
+                          ),
+                        )
                       ],
                     );
                   },
@@ -110,5 +117,9 @@ class _SolicitarVeiculoState extends State<SolicitarVeiculo> {
         testar.add(element.get("Nome"));
       });
     } catch (e) {}
+  }
+
+  atualizar() {
+    setState(() {});
   }
 }
